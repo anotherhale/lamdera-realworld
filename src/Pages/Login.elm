beec3d1120347e6b8a11f5c92257f9e852d5f7b5
Andrew Hale <AndrewHale@opalsoft.com>
2021-08-03 10:47:36 -0400

    added oauth2

--- src/Pages/Login.elm
+++ src/Pages/Login.elm
@@ -3,6 +3,7 @@ module Pages.Login exposing (Model, Msg(..), page)
 import Api.Data exposing (Data)
 import Api.User exposing (User)
 import Bridge exposing (..)
+import Browser.Navigation as Navigation exposing (Key)
 import Components.UserForm
 import Effect exposing (Effect)
 import Gen.Route as Route
@@ -11,17 +12,80 @@ import Request exposing (Request)
 import Shared
 import Utils.Route
 import View exposing (View)
-
+import Html exposing (button)
+import Html.Events exposing (onClick)
+import Html exposing (text)
+import Html.Attributes exposing (class)
+import Html exposing (div)
+import Json.Decode as Json
+import OAuth
+import OAuth.Implicit as OAuth
+import Url exposing (Protocol(..), Url)
 
 page : Shared.Model -> Request -> Page.With Model Msg
 page shared req =
     Page.advanced
-        { init = init shared
+        { init = init shared req
         , update = update req
         , subscriptions = subscriptions
         , view = view
         }
 
+
+
+type alias UserInfo =
+    { name : String
+    , picture : String
+    }
+
+
+type alias Configuration =
+    { authorizationEndpoint : Url
+    , userInfoEndpoint : Url
+    , userInfoDecoder : Json.Decoder UserInfo
+    , clientId : String
+    , scope : List String
+    }
+
+
+defaultHttpsUrl : Url
+defaultHttpsUrl =
+    { protocol = Https
+    , host = ""
+    , path = ""
+    , port_ = Nothing
+    , query = Nothing
+    , fragment = Nothing
+    }
+
+loginUrl : Url
+loginUrl = 
+    { protocol = Http
+    , host = "localhost"
+    , path = "signin"
+    , port_ = Just 9000
+    , query = Nothing
+    , fragment = Nothing
+    }
+{-| OAuth configuration.
+Note that this demo also fetches basic user information with the obtained access token,
+hence the user info endpoint and JSON decoder
+-}
+configuration : Configuration
+configuration =
+    { authorizationEndpoint =
+        { defaultHttpsUrl | host = "accounts.google.com", path = "/o/oauth2/v2/auth" }
+    , userInfoEndpoint =
+        { defaultHttpsUrl | host = "www.googleapis.com", path = "/oauth2/v1/userinfo" }
+    , userInfoDecoder =
+        Json.map2 UserInfo
+            (Json.field "name" Json.string)
+            (Json.field "picture" Json.string)
+    , clientId =
+        "909608474358-fkok86ks7e83c47aq01aiit47vsoh4s0.apps.googleusercontent.com"
+    , scope =
+        [ "profile" ]
+    }
 
 
 -- INIT
@@ -34,8 +98,8 @@ type alias Model =
     }
 
 
-init : Shared.Model -> ( Model, Effect Msg )
-init shared =
+init : Shared.Model -> Request -> ( Model, Effect Msg )
+init shared req =
     ( Model
         (case shared.user of
             Just user ->
@@ -57,6 +121,8 @@ init shared =
 type Msg
     = Updated Field String
     | AttemptedSignIn
+    | GoogleSignIn
+    | FacebookSignIn
     | GotUser (Data User)
 
 
@@ -75,6 +141,31 @@ update req msg model =
 
         Updated Password password ->
             ( { model | password = password }
+            , Effect.none
+            )
+
+        GoogleSignIn ->
+
+            let
+                state = "random string"
+                redirectUri = loginUrl
+                authorization =
+                    { clientId = configuration.clientId
+                    , redirectUri = redirectUri
+                    , scope = configuration.scope
+                    , state = Just state
+                    , url = configuration.authorizationEndpoint
+                    }
+            in
+            ( model
+            , Effect.fromCmd <| (authorization
+                |> OAuth.makeAuthorizationUrl
+                |> Url.toString
+                |> Navigation.load)
+            )
+
+        FacebookSignIn ->
+            ( model
             , Effect.none
             )
 
@@ -118,7 +209,17 @@ view : Model -> View Msg
 view model =
     { title = "Sign in"
     , body =
-        [ Components.UserForm.view
+        [ div [ class "container page" ]
+            [ div [ class "row" ] []
+            , div [ class "text-center"] [ button 
+                [ onClick GoogleSignIn, class "btn-google" ] [ text "Sign in" ] ]
+            ]
+        , div [ class "container page" ]
+            [ div [ class "row" ] []
+            , div [ class "text-center"] [ button 
+                [ onClick FacebookSignIn, class "btn-facebook" ] [ text "Sign in" ] ]
+            ]
+        , Components.UserForm.view
             { user = model.user
             , label = "Sign in"
             , onFormSubmit = AttemptedSignIn
@@ -137,4 +238,5 @@ view model =
                 ]
             }
         ]
+
     }
